38f7e04bd47b1d5f693fbbf1061ac6975eb2dc22
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-05 18:34:08 -0300

    feat: implement controller translate

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
+
 # from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from models.language_model import LanguageModel
+
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +12,15 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    languages = LanguageModel.find()
+    text_to_translate = "O que deseja traduzir"
+    translated = "Tradução"
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translated=translated,
+    )
 
 
 # Req. 6
