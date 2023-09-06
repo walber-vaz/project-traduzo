6017740526533bb19d5481d18e6e96bcda0372c1
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-06 10:31:34 -0300

    feat: implement test model history

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,23 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_request_history(prepare_base):
+    response = HistoryModel.list_as_json()
+    response = json.loads(response)
+    for item in response:
+        item.pop("_id")
+    assert response == [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
