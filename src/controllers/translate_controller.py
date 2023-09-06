3009788b986e7f9e7818214a1ad182538dec1131
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-06 09:36:34 -0300

    feat: implement controller reverse translate

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -10,17 +10,11 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])  # type: ignore
 def index():
-    if request.method == "GET":
-        languages = LanguageModel.find()
-        text_to_translate = "O que deseja traduzir"
-        translated = "Tradução"
-
-        return render_template(
-            "index.html",
-            languages=languages,
-            text_to_translate=text_to_translate,
-            translated=translated,
-        )
+    languages = LanguageModel.find()
+    text_to_translate = "O que deseja traduzir"
+    translated = "Tradução"
+    translate_from = "pt"
+    translate_to = "en"
 
     if request.method == "POST":
         text_to_translate = request.form.get("text-to-translate")
@@ -28,22 +22,39 @@ def index():
         translate_to = request.form.get("translate-to")
 
         translated_text = GoogleTranslator(
-            source=translate_from, target=translate_to
-        ).translate(text_to_translate)
-
-        languages = LanguageModel.find()
-
-        return render_template(
-            "index.html",
-            languages=languages,
-            text_to_translate=text_to_translate,
-            translate_from=translate_from,
-            translate_to=translate_to,
-            translated=translated_text,
+            source=translate_from, target=translate_to  # type: ignore
+        ).translate(
+            text_to_translate  # type: ignore
         )
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated_text if request.method == "POST" else translated,
+    )
 
 
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translator = GoogleTranslator(source=translate_from, target=translate_to)
+    translated_text = translator.translate(text_to_translate)
+
+    translate_from, translate_to = translate_to, translate_from
+    languages = LanguageModel.find()
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated_text,
+    )
